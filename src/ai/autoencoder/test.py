dcef560e930bcd1647a2196076fece341b0d8e36
vichShir <victor.shirasuna@gmail.com>
2025-03-03 16:40:50 -0300

    Added metrics in test.py for Autoencoder

--- src/ai/autoencoder/test.py
+++ src/ai/autoencoder/test.py
@@ -88,6 +88,22 @@ print(fn)
 print("FP:")
 print(fp)
 
+POS = len([v for k in gt.keys() for v in gt[k]])
+NEG = X_test.shape[0] - POS
+FP = len(fp)
+FN = len(fn)
+TP = POS - FP
+TN = NEG - FN
+# Compute precision, recall and F1-measure
+acc = 100 * (TP + TN) / (TP + TN + FP + FN)
+P = 100 * TP / (TP + FP)
+R = 100 * TP / (TP + FN)
+F1 = 2 * P * R / (P + R)
+fpr = 100 * FP / (FP + TN)
+tpr = 100 * TP / (TP + FN)
+print('false positive (FP): {}, false negative (FN): {}, Acc: {:.3f}%, Precision: {:.3f}%, Recall: {:.3f}%, F1-measure: {:.3f}%'.format(FP, FN, acc, P, R, F1))
+print('false positive rate: {:.3f}%, true positive rate: {:.3f}%'.format(fpr, tpr))
+
 # plot graph - reconstruction err w.r.t. to each sequence
 plot = True
 if plot:
