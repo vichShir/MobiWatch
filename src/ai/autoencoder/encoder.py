9c66c744341e1ae9850bb12a1e7cc6a11ac21cc4
vichShir <victor.shirasuna@gmail.com>
2025-03-03 16:30:29 -0300

    Fix import package relative

--- src/ai/autoencoder/encoder.py
+++ src/ai/autoencoder/encoder.py
@@ -1,9 +1,12 @@
+import sys
+sys.path.insert(1, '../../mobiflow')
+
 import pandas as pd
 import numpy as np
 import torch
 from torch import nn
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
-from .encoding import nas_emm_code_NR, rrc_dl_ccch_code_NR, rrc_dl_dcch_code_NR, rrc_ul_ccch_code_NR, rrc_ul_dcch_code_NR
+from encoding import nas_emm_code_NR, rrc_dl_ccch_code_NR, rrc_dl_dcch_code_NR, rrc_ul_ccch_code_NR, rrc_ul_dcch_code_NR
 
 class Encoder:
     def __init__(self):
